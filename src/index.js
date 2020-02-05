8ef5349e69d04abe8e8729fdf8e43c5584be4fe6
julizet <juliazet@gmx.de>
2020-02-05 12:36:10 +0100

    integrated osrm middleware

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ document.addEventListener('DOMContentLoaded', function() {
     preloader.style.display = 'none';
   });
 
-
   // TEXT ANIMATION -----
   var TxtRotate = function(el, toRotate, period) {
     this.toRotate = toRotate;
@@ -97,7 +96,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.appendChild(css);
   };
 
-
   // set bounds of the map -----
   map.setMaxBounds([[-180, -85], [180, 85]]);
 
@@ -106,7 +104,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // initalize a starting point for routing -----
   const start = [13.388443, 52.4839];
-
 
   // ++++ THIS WEHRE ROUTING FUNCTION STARTS ++++
 
@@ -129,67 +126,70 @@ document.addEventListener('DOMContentLoaded', function() {
     //   '?steps=true&geometries=geojson&access_token=' +
     //   mapboxgl.accessToken;
 
-    let url2 =
-      'http://osrm-docker-bikesharing-dev2.eu-central-1.elasticbeanstalk.com/route/v1/bicycle/' +
-      start[0] +
-      ',' +
-      start[1] +
-      ';' +
-      end[0] +
-      ',' +
-      end[1] +
-      '?geometries=geojson';
+    const data = {
+      type: 'bicycle',
+      start: start,
+      end: end,
+    };
 
-    console.log('API Url: ', url2);
-
-    // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-    let req = new XMLHttpRequest();
-    req.responseType = 'json';
-    req.open('GET', url2, true);
-    req.onload = function() {
-      let data = req.response.routes[0];
-      var route = data.geometry.coordinates;
-      var geojson = {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'LineString',
-          coordinates: route,
-        },
-      };
-      // if the route already exists on the map, reset it using setData
-      if (map.getSource('route')) {
-        map.getSource('route').setData(geojson);
-      } else {
-        // otherwise, make a new request
-        map.addLayer({
-          id: 'route',
-          type: 'line',
-          source: {
-            type: 'geojson',
-            data: {
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'LineString',
-                coordinates: geojson,
+    let url2 = 'https://osrm-middleware.now.sh/api';
+    fetch(url2, {
+      method: 'POST', //default would be GET
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        //get response in Json format
+        return response.json();
+      })
+      .then((myJson) => {
+        console.log('myJson: ', myJson);
+        var parsedJson = JSON.parse(myJson); //parse before using JSON
+        let route = parsedJson.routes[0].geometry.coordinates;
+        var geojson = {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: route, //route is an Array consisting of all the turning points on the route
+          },
+        };
+        // if the route already exists on the map, reset it using setData
+        if (map.getSource('route')) {
+          map.getSource('route').setData(geojson);
+        } else {
+          // otherwise, make a new request
+          map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: {
+              type: 'geojson',
+              data: {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                  type: 'LineString',
+                  coordinates: geojson,
+                },
               },
             },
-          },
-          layout: {
-            'line-join': 'round',
-            'line-cap': 'round',
-          },
-          paint: {
-            'line-color': '#11119f',
-            'line-width': 5,
-            'line-opacity': 0.65,
-          },
-        });
-      }
-      // add turn instructions here at the end
-    };
-    req.send();
+            layout: {
+              'line-join': 'round',
+              'line-cap': 'round',
+            },
+            paint: {
+              'line-color': '#11119f',
+              'line-width': 5,
+              'line-opacity': 0.65,
+            },
+          });
+        }
+      });
+    // add turn instructions here at the end
+    // SEND REQUEST
   }
 
   map.on('click', function(e) {
@@ -220,8 +220,6 @@ document.addEventListener('DOMContentLoaded', function() {
       },
     });
 
-    // get current GPS coordiantes of cursor --> first lng, then at
-    console.log('status: ', status);
     let coordsObj = e.lngLat;
     canvas.style.cursor = '';
     let coords = Object.keys(coordsObj).map(function(key) {
